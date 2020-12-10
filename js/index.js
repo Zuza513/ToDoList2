c06d7e02a96e4fa0ad9ab4a1f5850b38f3c6edac
Zuza Kępa <zuziagabrysiakepa@gmail.com>
2020-12-10 17:42:45 +0100

    Zajecia 9.12

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function changeInput() {
+    const toDoInput = document.getElementById("toDoInput");
+    const toDoButton = document.getElementById("inputButton")
+
+    if (toDoInput.value === ''){
+        toDoButton.style.display = "none";
+    } else {
+        toDoButton.style.display = "inline";
+    }
+}
