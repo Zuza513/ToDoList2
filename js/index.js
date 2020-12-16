c8fc1297da76183e929777f2c88b29a5cab6bd3b
Zuza Kępa <zuziagabrysiakepa@gmail.com>
2020-12-16 19:32:22 +0100

    Zajecia 16.12

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function changeInput() {
+function changeInput(event) {
     const toDoInput = document.getElementById("toDoInput");
     const toDoButton = document.getElementById("inputButton")
 
@@ -7,5 +7,60 @@ function changeInput() {
     } else {
         toDoButton.style.display = "inline";
     }
+
+    if (event.keyCode === 13){
+        addTodo();
+    }
 }
+
+function addTodo() {
+    const toDoInput = document.getElementById("toDoInput");
+    const toDoListContainer = document.getElementById("toDoListContainer");
+    const toDoContainers = document.getElementsByClassName("toDoContainer");
+    const inputButton = document.getElementById("inputButton");
+
+    toDoListContainer.innerHTML +=
+        ` <div class="toDoContainer" id="toDoContainer${toDoContainers.length + 1}">
+            <button class="toDoButton" id="toDoButton${toDoContainers.length + 1}" onclick="deleteTodo(${toDoContainers.length + 1})">x</button>
+            <input type="checkbox" class="toDoCheckbox" id="toDoCheckbox${toDoContainers.length + 1}" onchange="changeCheckbox(${toDoContainers.length + 1})">
+            <label class="toDoLabel" id="toDoLabel${toDoContainers.length + 1}">${toDoInput.value}</label>
+        </div>`
+
+        toDoInput.value = '';
+        inputButton.style.display = 'none';
+
+        const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+
+        todos.push({id: 1, todo:"Tekst", done: false});
+
+        localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function deleteTodo(id) {
+    const container = document.getElementById(`toDoContainer${id}`);
+    container.remove();
+}
+
+function changeCheckbox(id) {
+    const checkbox = document.getElementById(`toDoCheckbox${id}`);
+    const label = document.getElementById(`toDoLabel${id}`);
+
+    if (checkbox.checked) {
+        label.classList.add("toDoLabelCross");
+    } else {
+        label.classList.remove("toDoLabelCross");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
