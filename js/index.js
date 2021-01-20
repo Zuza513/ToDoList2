975834d56505321772bc068c2ca3293471744705
mkepa <marcin.kepa@grandparade.co.uk>
2021-01-20 19:36:03 +0100

    zajecia z tata

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-window.onload = function() {
-    const todos = JSON.parse(localStorage.getItem("todos") || '[]' );
+window.onload = function () {
+    const todos = JSON.parse(localStorage.getItem("todos") || '[]');
     const toDoListContainer = document.getElementById("toDoListContainer");
 
-    for (let i = 0; i < todos.length; i++){
+    for (let i = 0; i < todos.length; i++) {
         const id = todos[i].id;
         const todo = todos[i].todo;
+        let labelClasses;
+        if (todos[i].done){
+            labelClasses = 'toDoLabel toDoLabelCrossed';
+        } else {
+            labelClasses = 'toDoLabel';
+        }
 
         toDoListContainer.innerHTML +=
-        ` <div class="toDoContainer" id="toDoContainer${id}">
-        <button class="toDoButton" id="toDoButton${id}" onclick="deleteTodo(${id})">x</button>
-        <input type="checkbox" class="toDoCheckbox" id="toDoCheckbox${id}" onchange="changeCheckbox(${id})">
-        <label class="toDoLabel" id="toDoLabel${id}">${toDoInput.value}</label>
-    </div>`
+            ` <div class="toDoContainer" id="toDoContainer${id}">
+                 <button class="toDoButton" id="toDoButton${id}" onclick="deleteTodo(${id})">x</button>
+                 <input type="checkbox" class="toDoCheckbox" id="toDoCheckbox${id}" onchange="changeCheckbox(${id})">
+                 <label class="${labelClasses}" id="toDoLabel${id}">${todo}</label>
+             </div>`
 
     }
 }
@@ -20,13 +26,13 @@ function changeInput(event) {
     const toDoInput = document.getElementById("toDoInput");
     const toDoButton = document.getElementById("inputButton")
 
-    if (toDoInput.value === ''){
+    if (toDoInput.value === '') {
         toDoButton.style.display = "none";
     } else {
         toDoButton.style.display = "inline";
     }
 
-    if (event.keyCode === 13){
+    if (event.keyCode === 13) {
         addTodo();
     }
 }
@@ -45,28 +51,28 @@ function addTodo() {
         </div>`
 
 
-        // todos = [
-         //  {id: 1, todo: "jakies zadanie", done: true},
-         // {id: 2, todo: " drugie jakies", done: true },
-         // {id: 3, todo: " drugie jakies", done: true },
-         // {id: 3, todo: " drugie jakies", done: true },
-        //]
+    // todos = [
+    //  {id: 1, todo: "jakies zadanie", done: true},
+    // {id: 2, todo: " drugie jakies", done: true },
+    // {id: 3, todo: " drugie jakies", done: true },
+    // {id: 3, todo: " drugie jakies", done: true },
+    //]
 
-        const todos = JSON.parse(localStorage.getItem("todos") || "[]");
-        let currentIndex;
+    const todos = JSON.parse(localStorage.getItem("todos") || "[]");
+    let currentIndex;
 
-        if(todos.length == 0) {
-            currentIndex = 0;
-        } else {
-            currentIndex = todos[todos.length -1].id;
-                }
+    if (todos.length === 0) {
+        currentIndex = 0;
+    } else {
+        currentIndex = todos[todos.length - 1].id;
+    }
 
-        todos.push({id: currentIndex + 1, todo: toDoInput.value, done: false});
+    todos.push({id: currentIndex + 1, todo: toDoInput.value, done: false});
 
-        localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 
-        toDoInput.value = '';
-        inputButton.style.display = 'none';
+    toDoInput.value = '';
+    inputButton.style.display = 'none';
 }
 
 function deleteTodo(id) {
@@ -74,6 +80,14 @@ function deleteTodo(id) {
     container.remove();
 
     const todos = JSON.parse(localStorage.getItem('todos'));
+
+    for (let i = 0; i < todos.length; i++){
+        if(todos[i].id === id){
+            todos.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function changeCheckbox(id) {
@@ -85,17 +99,18 @@ function changeCheckbox(id) {
     } else {
         label.classList.remove("toDoLabelCross");
     }
+
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    for (let i = 0; i < todos.length; i++){
+        if(todos[i].id === id){
+            if(todos[i].done === true){
+                todos[i].done = false;
+            } else {
+                todos[i].done = true;
+            }
+        }
+    }
+    localStorage.setItem("todos", JSON.stringify(todos));
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
